b83517b1a95de7ed760796441de4e486894d8745
Ahmed Esmail <ahmdesmaill@proton.me>
2026-07-02 15:17:07 +0300

    fix: replace the sed command with a simple echo to make it work on macOS

--- 08e-rebase-edit/setup.sh
+++ 08e-rebase-edit/setup.sh
@@ -2,9 +2,9 @@
 rm -rf .git *.py
 git init -q
 echo 'print("Start")' > app.py
-echo '' >> app.py
 git add . && git commit -q -m 'Initial'
-sed -i '2i print("Helo")' app.py 
+echo 'print("Helo")' >> app.py
+echo '' >> app.py
 git add . && git commit -q -m 'Add greeting with typo'
 echo 'print("End")' >> app.py
 git add . && git commit -q -m 'Add end line'
